36c58513b5f54ed549de46120b95ebe3df052557
Jin Zhang <37540255+xiaobu8806@users.noreply.github.com>
2021-04-06 17:33:48 +0800

    feat(ADD): add SingletomPattern demo

--- src/main/java/ConfigurationParse.java
+++ src/main/java/ConfigurationParse.java
@@ -8,6 +8,9 @@ public class ConfigurationParse {
     private static final Logger logger = LogManager.getLogger(ConfigurationParse.class);
 
     public static void main(String[] args) {
+        if (args.length < 1) {
+            throw new RuntimeException("args can't be null");
+        }
         logger.info("NTM");
         SingletonPattern pattern = SingletonPattern.getInstance();
         pattern.check(args[0]);
